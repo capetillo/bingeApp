2a0c8e6251b44e219f851fbe982dce2a4d37ab67
carolina <carolinacapetilloc@gmail.com>
2023-03-08 15:18:01 -0800

    added typedefs folder, resolvers folder, and passed these two as parameters in index.js for server to listen and display

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const { ApolloServer } = require("apollo-server")
 
-const server = new ApolloServer()
+const server = new ApolloServer({typeDefs, resolvers});
+// resolvers are functions that deal with the data w graphql
+
+server.listen().then(({url}) => {
+    console.log(`YOUR API IS RUNNING AT: ${url}`)
+});
