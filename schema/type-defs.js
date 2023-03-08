74a5b3f33bec4a2959007f500311d08bad74c0ec
carolina <carolinacapetilloc@gmail.com>
2023-03-08 15:31:50 -0800

    defined types of user and query

--- schema/type-defs.js
+++ schema/type-defs.js
@@ -1,7 +1,18 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
-    type Query {
-        
+
+    type User {
+        id: ID!
+        name: String!
+        username: String!
+      #for now, movies is string but movies will be defined later
+        movies: String!
+
     }
-`
+
+# first level 
+    type Query {
+        users: [User!]!  
+    }
+`;
