932f991013c79e5470beebadf511cde29b88a364
carolina <carolinacapetilloc@gmail.com>
2023-03-09 15:45:06 -0800

    added lodash liash library and resolver returns single user

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,15 +1,22 @@
 const { UserList } = require("../FakeData")
-// will contain all of the resolver functions needed to retrieve data from api
+const _ = require("lodash");
+
+// will co ntain all of the resolver functions needed to retrieve data from api
+
 const resolvers = {
     Query: {
         // here we define all of the resolver functions that will exist as a subfield for the Query type
         // right now it is only users 
         // here we make the api call 
-        users() {
+        users: () => {
             return UserList;
-        } 
-    }
-
-}
+        },
+        user: (parent, args) => {
+            const id = args.id
+            const user = _.find(UserList, { id })
+            return user;
+        },
+    },
+};
 
 module.exports = { resolvers };
